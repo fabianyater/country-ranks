81e49e8449de79e77ce2c3f6426f116e9d51d7ce
Fabian Yate <fabianyate31@gmail.com>
2024-06-06 00:25:55 -0500

    add function to capitalize a word and return a value of SortType

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,36 @@
+import { Country, SortType } from "./types/CountryType";
+
 export const addPluralText = (totalCountries: number): string => {
   return `Found ${totalCountries} ${
     totalCountries === 1 ? "country" : "countries"
   }`;
 };
 
-export const capitalize = (word: string): string => {
-  if (!word) return "";
-  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+export const capitalize = (word: string): SortType => {
+  if (!word) throw new Error("Invalid input: word cannot be empty");
+
+  const capitalized =
+    word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+
+  if (["Population", "Area", "Name"].includes(capitalized)) {
+    return capitalized as SortType;
+  }
+
+  throw new Error(`Invalid input: "${capitalized}" is not a valid SortType`);
+};
+
+export const sortCountries = (countries: Country[], sortType: SortType) => {
+  const compareFunctions: {
+    [key in SortType]: (a: Country, b: Country) => number;
+  } = {
+    Population: (a: Country, b: Country) => b.population - a.population,
+    Area: (a: Country, b: Country) => b.area - a.area,
+    Name: (a: Country, b: Country) =>
+      a.name.common.localeCompare(b.name.common),
+  };
+
+  const compare = compareFunctions[sortType];
+  return countries.slice().sort(compare);
 };
 
 export const sortOptions = [
