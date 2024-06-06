a8c6f63fbbfa8423a1f39746827c0955363af6ed
Fabian Yate <fabianyate31@gmail.com>
2024-06-05 23:32:56 -0500

    Add table to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from "./assets/Logo.svg";
 import bgImage from "./assets/hero-image-wr.jpg";
 import Aside from "./components/Aside";
 import SearchInput from "./components/SearchInput";
+import Table from "./components/Table";
 import { useCountryContext } from "./hooks/useCountry";
 import { addPluralText } from "./utils";
 
@@ -10,8 +11,8 @@ function App() {
   const { totalCountries } = useCountryContext();
   return (
     <>
-      <div style={{ position: "absolute", zIndex: "-1" }}>
-        <img src={bgImage} alt="" />
+      <div className={styles.backgoundImage}>
+        <img src={bgImage} alt="Image of the planet form space" />
       </div>
       <div className={styles.container}>
         <header className={styles.header}>
@@ -26,7 +27,9 @@ function App() {
           </section>
           <section className={styles.content__body}>
             <Aside />
-            <section></section>
+            <section className={styles.table_wrapper}>
+              <Table />
+            </section>
           </section>
         </main>
       </div>
